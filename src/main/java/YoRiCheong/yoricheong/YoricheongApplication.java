1afbc6cd73be3fb91a519dff111ae13a41b001e5
pengzhongjie <pengzhongjie@meizu.com>
2021-12-30 16:23:14 +0800

    Initial commit

---
+++ src/main/java/YoRiCheong/yoricheong/YoricheongApplication.java
@@ -0,0 +1,13 @@
+package YoRiCheong.yoricheong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoricheongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoricheongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/YoRiCheong/yoricheong/YoricheongApplicationTests.java
@@ -0,0 +1,13 @@
+package YoRiCheong.yoricheong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoricheongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
